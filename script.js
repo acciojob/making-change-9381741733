0bcc7cf6bcb967cd9188fe399ca3be77a85e9597
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-03-16 18:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const makeChange = (c) => {
-  // your name here
+  let q = Math.floor(c / 25); // Get number of quarters
+  c %= 25; // Get the remaining amount after quarters
+  
+  let d = Math.floor(c / 10); // Get number of dimes
+  c %= 10; // Get the remaining amount after dimes
+  
+  let n = Math.floor(c / 5); // Get number of nickels
+  c %= 5; // Get the remaining amount after nickels
+  
+  let p = c; // Remaining cents are pennies
+  
+  return { q, d, n, p };
 };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
+// Do not change the code below
+const c = parseInt(prompt("Enter c: "), 10);
 alert(JSON.stringify(makeChange(c)));
